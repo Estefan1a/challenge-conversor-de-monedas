f6d3049419b4332ce51bf9ddabfbf7949a40ac55
Estefanía González <estefania.gonzre@gmail.com>
2025-08-07 20:32:19 -0600

    Actualiza Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,82 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-
+        int opcion = 0;
         Scanner teclado = new Scanner(System.in);
         ConsultaMoneda consulta = new ConsultaMoneda();
-        System.out.println("Ingrese el tipo de conversión que quiere realizar: USD, MXM");
+        ConversionDeMoneda conversion = new ConversionDeMoneda();
 
-        var tipoDeMoneda = teclado.nextLine();
-        Moneda moneda = consulta.buscarMoneda(tipoDeMoneda);
+        String menu = """
+            Incerta la conversión que deseas realizar
+            
+            1.- Convertir de Dólares a Pesos Mexicanos
+            2.- Convertir de Pesos Mexinanos a Dólares
+            3.- Convertir de Dólares a Pesos Argentinos
+            4.- Convertir de Pesos Argentinos a Dólares
+            5.- Convertir de Dólares a Pesos Colombianos
+            6.- Convertir de Pesos Colombianos a Dólares
+            7.- Convertir de Dólares a Reales Brasileños
+            8.- Convertir de Pesos Reales Brasileños a Dólares
+            9.- Salir
+            """;
 
-        System.out.println(moneda);
 
+        while (true){
+            try {
+                System.out.println(menu);
+                opcion = teclado.nextInt();
+
+                if(opcion == 9){
+                    break;
+                }
+
+                System.out.println("Incerte la cantidad que desea convertir: ");
+                double cantidadAConvertir = teclado.nextDouble();
+
+                switch (opcion) {
+                    case 1:
+                        conversion.convertir("USD", "MXN", cantidadAConvertir);
+                        break;
+                    case 2:
+                        conversion.convertir("MXN", "USD", cantidadAConvertir);
+                        break;
+                    case 3:
+                        conversion.convertir("USD", "ARS", cantidadAConvertir);
+                        break;
+                    case 4:
+                        conversion.convertir("ARS", "USD", cantidadAConvertir);
+                        break;
+                    case 5:
+                        conversion.convertir("USD", "COP", cantidadAConvertir);
+                        break;
+                    case 6:
+                        conversion.convertir("COP", "USD", cantidadAConvertir);
+                        break;
+                    case 7:
+                        conversion.convertir("USD", "BRL", cantidadAConvertir);
+                        break;
+                    case 8:
+                        conversion.convertir("BRL", "USD", cantidadAConvertir);
+                        break;
+                    default:
+                        System.out.println("Por favor ingrese una opción válida");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debes ingresar un número válido.");
+                teclado.nextLine(); // limpia la entrada inválida para evitar el bucle infinito
+            } catch (NumberFormatException e) {
+                System.out.println("Error, el formato del número es inválido " + e.getMessage());
+            } catch (NullPointerException e) {
+                System.out.println("Se intentó usar una variable que es null." + e.getMessage());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("FINALIZANDO LA APLICACIÓN");
+            }
+        }
+        teclado.close();
     }
-
-
 }
+
